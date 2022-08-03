1c45fd9f90b23bdb998ece524e6f9c6727bcf861
SaraRassa <107958695+SaraRassa@users.noreply.github.com>
2022-08-03 13:53:36 -0600

    Delete Clip.py

--- Clip.py
+++
@@ -1,7 +0,0 @@
-from Media import Media
-class Clip(Media):
-    def __init__(self, code, name, director, IMDB=None, URL=None, duration=None, cast=None):
-        Media.__init__(self, code, name, director, IMDB, URL, duration, cast)
-    
-    def show_info(self):
-        Media.show_info(self)
