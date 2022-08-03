38ca9445034207594684ace4a4ff11f967e4fba3
SaraRassa <107958695+SaraRassa@users.noreply.github.com>
2022-08-03 13:54:45 -0600

    Delete Movie.py

--- Movie.py
+++
@@ -1,9 +0,0 @@
-from Media import Media
-class Movie(Media):
-    def __init__(self, code, name, director, IMDB, URL, duration, cast, genre):
-        Media.__init__(self,code, name, director, IMDB, URL, duration, cast)
-        self.genre= genre
-    
-    def show_info(self):
-        Media.show_info(self)
-        print('Genre: ',self.genre)
